86b27feeb72690ca0f7576cfc0da47669dc67eb8
Claas Augner <caugner@mozilla.com>
2023-07-04 10:12:55 +0200

    chore(ai-help): update thumbs copy

--- client/src/plus/ai-help/index.tsx
+++ client/src/plus/ai-help/index.tsx
@@ -231,9 +231,9 @@ export function AIHelpInner() {
                             )}
                             <GleanThumbs
                               feature="ai-help-answer"
-                              question={"Is this answer useful?"}
-                              upLabel={"Yes, this answer is useful."}
-                              downLabel={"No, this answer is not useful."}
+                              question={"Was this answer useful?"}
+                              upLabel={"Yes, this answer was useful."}
+                              downLabel={"No, this answer was not useful."}
                               permanent={true}
                             />
                           </>
